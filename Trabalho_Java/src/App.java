fb32b73a9389a5158a81dfb38ff3576f971a35f9
Marcos Pedro de Macêdo <marcospedromdmacedo@outlook.com>
2024-06-10 20:44:20 -0300

    Criação da classe ListarTodos, e adição das funções na App.

--- Trabalho_Java/src/App.java
+++ Trabalho_Java/src/App.java
@@ -1,27 +1,41 @@
 import java.util.Scanner;
 
+import Classes.Adicionar;
+import Classes.ListarTodos;
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         int escolha = 0;
+        Adicionar listarAdcionar = new Adicionar();
+        ListarTodos listarUsuarios = new ListarTodos();
         
 
         do {
             System.out.println("""
+
+
                 Escolha uma das opções abaixo: 
                 1. Adcionar
                 2. Listar Todos.
                 3. Alterar 
                 4. Excluir
+
+
                 """);
             escolha = leitor.nextInt();
 
             switch (escolha) {
                 case 1:
-                    
+                    String frase = listarAdcionar.Printar();
+                    System.out.println(frase);
+                    escolha = 0;
                     break;
                 case 2:
-
+                    String fraseUser = listarUsuarios.ListarTodosDaLista(); 
+                    System.out.println(fraseUser);
+                    escolha = 0;
                     break;
 
                 case 3: 
